28a94f6749f4c120c6618a69a4d5cf917c2ad4c5
yqp <468417415@qq.com>
2020-04-15 17:21:54 +0800

    config pack;and push to fir.im

--- src/pages/welcome/WelcomePage.js
+++ src/pages/welcome/WelcomePage.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image,StatusBar} from 'react-native';
+import LinearGradient from "react-native-linear-gradient";
 import {connect} from 'react-redux';
 
 
@@ -13,9 +14,20 @@ class WelcomePage extends Component {
   
   render() {
     return (
+      <LinearGradient
+      useAngle={true}
+      angle={-45}
+      angleCenter={{x:0,y:0}}
+      colors={['#FF6666','#FFFF99']}
+      style={styles.container}>
+    <StatusBar hidden={true} />
     <View style={styles.container}>
-      <Text style={styles.text}>smileyqp</Text>
+      <Text style={styles.text}>welcome to React Native</Text>
+      <Text style={styles.text}>by Smileyqp</Text>
+
     </View>
+
+  </LinearGradient>
     );
   }
 }
@@ -36,8 +48,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor:'pink'
-  }
+  },
+  text: {
+    fontSize: 30,
+    color: '#FFFFFF',
+  },
 })
 
 
