98e269e549d8174cd3568b6d3a040b089d37f314
yqp <468417415@qq.com>
2020-04-15 15:08:21 +0800

    smileyqp

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Image,StatusBar} from 'react-native';
+import {connect} from 'react-redux';
+import DynamicTabNavigator from '../navigator/DynamicTabNavigator'
+
+
+export default class HomePage extends Component{
+    constructor(props){
+        super(props)
+        console.disableYellowBox = true;
+    }
+    render(){
+        return <DynamicTabNavigator/>
+    }
+}
